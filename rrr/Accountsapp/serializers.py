739ac5ed2b72441f6cae54ab80ab5d252ff69138
Abhishek Yalla <yallaabhishek7890@gmail.com>
2026-05-10 11:24:12 +0530

    Delete rrr directory

--- rrr/Accountsapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import Registration
-from rest_framework import serializers
-
-class RegistrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Registration
-        fields = '__all__'
